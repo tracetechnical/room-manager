5d077317327e4737594ec1da48aeebf26fe1d234
Alex Bush <alex.pheonixtechnical@gmail.com>
2021-05-23 18:49:34 +0100

    feat: add spring web; feat: upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,42 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.4"
+    id("org.springframework.boot") version "2.5.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.31"
-    kotlin("plugin.spring") version "1.4.31"
+    kotlin("jvm") version "1.5.0"
+    kotlin("plugin.spring") version "1.5.0"
 }
 
-group = "uk.co.tracetechnical"
+group = "uk.co.tracetechnicalservices"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
+    maven { url = uri("https://repo.spring.io/milestone") }
 }
 
+extra["springCloudVersion"] = "2020.0.3-SNAPSHOT"
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("io.reactivex.rxjava2:rxjava:2.2.20")
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/uk/co/tracetechnicalservices/roommanager/RoomManagerApplication.kt
@@ -0,0 +1,11 @@
+package uk.co.tracetechnicalservices.roommanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoomManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoomManagerApplication>(*args)
+}

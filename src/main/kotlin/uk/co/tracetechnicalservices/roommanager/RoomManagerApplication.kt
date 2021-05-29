9a05a59b3d920a65ec7c8b6e5f8cf269927c041d
Alex Bush <alex.pheonixtechnical@gmail.com>
2021-05-29 17:24:14 +0100

    feat: add LR tv off; chore: remove souts

--- src/main/kotlin/uk/co/tracetechnicalservices/roommanager/RoomManagerApplication.kt
+++ src/main/kotlin/uk/co/tracetechnicalservices/roommanager/RoomManagerApplication.kt
@@ -11,39 +11,46 @@ class RoomManagerApplication
 fun main(args: Array<String>) {
     runApplication<RoomManagerApplication>(*args)
     val mqttService = MqttService()
-    val on = PublishSubject.create<MqttMessage>()
-    val off = PublishSubject.create<MqttMessage>()
+    val onLiving = PublishSubject.create<MqttMessage>()
+    val offLiving = PublishSubject.create<MqttMessage>()
+    val offLongLiving = PublishSubject.create<MqttMessage>()
     val onDining = PublishSubject.create<MqttMessage>()
     val offDining = PublishSubject.create<MqttMessage>()
-    mqttService.registerListener("lighting/switches/1/short", on)
-    mqttService.registerListener("lighting/switches/2/short", off)
+
+    mqttService.registerListener("lighting/switches/1/short", onLiving)
+    mqttService.registerListener("lighting/switches/2/short", offLiving)
+    mqttService.registerListener("lighting/switches/2/long", offLongLiving)
     mqttService.registerListener("lighting/switches/3/short", onDining)
     mqttService.registerListener("lighting/switches/4/short", offDining)
-    on.subscribe { a: MqttMessage ->
+
+    onLiving.subscribe { a: MqttMessage ->
         if (a.toString() == "1") {
-            println("Dim Up")
             mqttService.publish("lighting/dimmerGroup/3/level", "1024")
             mqttService.publish("lighting/dimmerGroup/4/level", "1024")
             mqttService.publish("lighting/dimmerGroup/5/level", "1024")
         }
     }
-    off.subscribe { a: MqttMessage ->
+    offLiving.subscribe { a: MqttMessage ->
         if (a.toString() == "1") {
-            println("Dim Down Off")
             mqttService.publish("lighting/dimmerGroup/3/level", "0")
             mqttService.publish("lighting/dimmerGroup/4/level", "0")
             mqttService.publish("lighting/dimmerGroup/5/level", "0")
         }
         if (a.toString() == "2") {
-            println("Dim Down Low")
             mqttService.publish("lighting/dimmerGroup/3/level", "10")
             mqttService.publish("lighting/dimmerGroup/4/level", "10")
             mqttService.publish("lighting/dimmerGroup/5/level", "10")
         }
     }
+    offLongLiving.subscribe { a: MqttMessage ->
+        if (a.toString() == "1") {
+            mqttService.publish("lighting/dimmerGroup/3/level", "10")
+            mqttService.publish("lighting/dimmerGroup/4/level", "0")
+        }
+    }
+
     onDining.subscribe { a: MqttMessage ->
         if (a.toString() == "1") {
-            println("Dim Up")
             mqttService.publish("lighting/dimmerGroup/6/level", "1024")
             mqttService.publish("lighting/dimmerGroup/7/level", "1024")
             mqttService.publish("lighting/dimmerGroup/8/level", "1024")

659eb3850e1875f1b77ba5075780b6a7c8c6eed8
Alex Bush <alex.pheonixtechnical@gmail.com>
2021-06-03 15:25:04 +0100

    feat: add CORS

--- src/main/kotlin/uk/co/tracetechnicalservices/roommanager/RoomManagerApplication.kt
+++ src/main/kotlin/uk/co/tracetechnicalservices/roommanager/RoomManagerApplication.kt
@@ -2,10 +2,22 @@ package uk.co.tracetechnicalservices.roommanager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @SpringBootApplication
-class RoomManagerApplication
+class RoomManagerApplication {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("*")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<RoomManagerApplication>(*args)

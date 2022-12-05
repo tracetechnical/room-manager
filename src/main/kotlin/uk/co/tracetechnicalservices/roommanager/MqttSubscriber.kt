012fe3f123d82f448a2277f04677234fa75ea62c
Alex Bush <alex.pheonixtechnical@gmail.com>
2022-12-05 00:20:49 +0000

    feat: add conn loss detection

--- src/main/kotlin/uk/co/tracetechnicalservices/roommanager/MqttSubscriber.kt
+++ src/main/kotlin/uk/co/tracetechnicalservices/roommanager/MqttSubscriber.kt
@@ -6,7 +6,10 @@ import org.eclipse.paho.client.mqttv3.MqttCallback
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 
 class MqttSubscriber(var listeners: Map<String, PublishSubject<MqttMessage>>) : MqttCallback {
-    override fun connectionLost(throwable: Throwable) {}
+    override fun connectionLost(throwable: Throwable) {
+        println("Lost connection, exiting to restart service")
+        System.exit(1)
+    }
 
     override fun messageArrived(path: String, mqttMessage: MqttMessage) {
         val pub = listeners[path]
